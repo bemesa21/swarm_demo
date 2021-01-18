12957951074ea98390627715e8013379cd43b373
Berenice medel <berenice@bunsan.io>
2021-01-18 13:41:32 -0600

    Add Libcluster confand application callbacks

---
+++ lib/swarm_demo.ex
@@ -0,0 +1,29 @@
+defmodule SwarmDemo do
+  use Application
+
+  def start(_type, _args) do
+    topologies = [
+      my_topology: [
+        strategy: Cluster.Strategy.Gossip,
+        config: [
+          port: 45892,
+          if_addr: "0.0.0.0",
+          multicast_if: "192.168.1.1",
+          multicast_addr: "230.1.1.251",
+          multicast_ttl: 1,
+          secret: "somepassword"],
+      ]
+    ]
+
+    children = [
+      {Cluster.Supervisor, [topologies, [name: SwarmDemo.ClusterSupervisor]]}
+    ]
+
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: SwarmDemo.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+
+end
